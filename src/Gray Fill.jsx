03559a6163c6a39f465af29e5c7730ffcbe40e6c
smorodsky <smorodskiy@kosmo.local>
2013-02-07 18:40:32 +0400

    Gray objects
    add two commands for select grayed objects

---
+++ src/Gray Fill.jsx
@@ -0,0 +1,9 @@
+﻿// Выделение всех объектов с заливкой Gray.
+
+#target "illustrator"
+#include "ProgressBar.incjsx"
+#include "Select.incjsx"
+        
+Select('pathItems', function(pi){
+    return pi.filled && pi.fillColor.typename == 'GrayColor';        
+});
---
+++ src/Gray Stroke.jsx
@@ -0,0 +1,9 @@
+﻿// Выделение всех объектов с обводкой Gray.
+
+#target "illustrator"
+#include "ProgressBar.incjsx"
+#include "Select.incjsx"
+        
+Select('pathItems', function(pi){
+    return pi.stroked && pi.strokeColor.typename == 'GrayColor';        
+});
